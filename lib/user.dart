c412eb732b5ff3810b8518fd326aed5c5bf6ebba
ZARIFMAC <uzbzarif@gmail.com>
2022-10-25 15:37:56 +0500

    Constructor from JSON

--- lib/user.dart
+++ lib/user.dart
@@ -2,6 +2,14 @@ class User {
   String? username;
   String? firstname;
   String? lastname;
+
   // Define a constructor
   User({this.username, this.firstname, this.lastname});
+
+  // Define a constructor to convert the JSON to User object
+  User.fromJson(Map<String, dynamic> json) {
+    username = json['username'];
+    firstname = json['firstname'];
+    lastname = json['lastname'];
+  }
 }
